43084dbdf0ab5d67938df7e364fbf623c2f2797e
Feby Desna Yunita <febydesna@gmail.com>
2022-12-15 19:27:32 +0700

    Deploy

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://finalprojectdeploy.web.app" // TODO: replace this
 const studentName = "Feby Desna Yunita" // TODO: replace this
 const studentId = "FE3224469" // TODO: replace this
 
